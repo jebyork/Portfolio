89cd333befdc7c16700043ac404a8e71af0190c3
joshb <joshbrownsword@icloud.com>
2024-09-13 15:56:49 +0100

    Portfolio| basic setup of layout of my portfolio| 13/09/2024

---
+++ scripts.js
@@ -0,0 +1,19 @@
+﻿function toggleMobileMenu() {
+    document.getElementById("menu").classList.toggle("active");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const skillBars = document.querySelectorAll(`.skill-bar`);
+
+    skillBars.forEach(bar => {
+        const skillName = bar.getAttribute("skill-name");
+        const rating = bar.getAttribute(`data-rating`);
+        const percentageRating = rating * 10;
+
+        const infoElement = bar.querySelector(`.info`);
+        const ratingBar = bar.querySelector(`.rating-bar`);
+
+        infoElement.textContent = `${skillName} ${rating}/10`;
+        ratingBar.style.width = `${percentageRating}%`;
+    });
+});
